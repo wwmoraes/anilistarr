1b35960d3786f5a1f6b9c26581ef7a7fbe3b752e
William Artero <git@artero.dev>
2023-08-14 17:21:16 +0200

    refactor: abstract filesystem from the JSONLocalPath adapter

--- internal/adapters/jsonlocalpath.go
+++ internal/adapters/jsonlocalpath.go
@@ -3,18 +3,21 @@ package adapters
 import (
 	"context"
 	"fmt"
-	"os"
+	"io/fs"
 
 	"github.com/wwmoraes/anilistarr/internal/telemetry"
 )
 
-type JSONLocalPath[F Metadata] string
+type JSONLocalPath[F Metadata] struct {
+	Fs   fs.FS
+	Name string
+}
 
 func (source JSONLocalPath[F]) Fetch(ctx context.Context) ([]Metadata, error) {
 	_, span := telemetry.StartFunction(ctx)
 	defer span.End()
 
-	data, err := os.ReadFile(string(source))
+	data, err := fs.ReadFile(source.Fs, source.Name)
 	if err != nil {
 		return nil, span.Assert(fmt.Errorf("failed to read local JSON: %w", err))
 	}
