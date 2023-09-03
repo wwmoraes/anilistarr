a969b234fdfe482551982216961a8c8fcaeb25e9
William Artero <git@artero.dev>
2023-09-03 13:27:20 +0200

    test: added JSON local provider unit tests

---
+++ internal/adapters/jsonlocalprovider_test.go
@@ -0,0 +1,60 @@
+package adapters_test
+
+import (
+	"context"
+	"reflect"
+	"strconv"
+	"testing"
+
+	"github.com/wwmoraes/anilistarr/internal/adapters"
+	"github.com/wwmoraes/anilistarr/internal/test"
+)
+
+var (
+	sampleData = `[
+		{"anilist_id": 1, "thetvdb_id": 91},
+		{"anilist_id": 2, "thetvdb_id": 92}
+	]`
+	testProvider adapters.JSONLocalProvider[memoryMetadata] = adapters.JSONLocalProvider[memoryMetadata]{
+		Fs: &test.MemoryFS{
+			"test.json": []byte(sampleData),
+		},
+		Name: "test.json",
+	}
+)
+
+type memoryMetadata struct {
+	AnilistID uint64 `json:"anilist_id"`
+	TheTvdbID uint64 `json:"thetvdb_id"`
+}
+
+func (entry memoryMetadata) GetSourceID() string {
+	return strconv.FormatUint(entry.AnilistID, 10)
+}
+
+func (entry memoryMetadata) GetTargetID() string {
+	return strconv.FormatUint(entry.TheTvdbID, 10)
+}
+
+func TestJSONLocalProvider(t *testing.T) {
+	mapper := &adapters.Mapper{
+		Provider: testProvider,
+		Store:    &test.Store{},
+	}
+
+	ctx := context.Background()
+	err := mapper.Refresh(ctx, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wantedTargetIds := []string{"91", "92"}
+	targetIds, err := mapper.MapIDs(ctx, []string{"1", "2", "3"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(targetIds, wantedTargetIds) {
+		t.Fatalf("mapped IDs don't match: got %v, wanted %v", targetIds, wantedTargetIds)
+	}
+}
