d3eaee480e7a9a589f81e31b792b589ad4b668ea
William Artero <git@artero.dev>
2024-07-07 19:15:43 +0200

    chore: sayonara GNU Make, hello Taskfile
    Signed-off-by: William Artero <git@artero.dev>

--- .make/codecov.mk
+++
@@ -1,9 +0,0 @@
-CODECOV ?= codecov
-CODECOV_FLAGS ?= -X fixes
-CODECOV_TOKEN ?=
-
-.PHONY: codecov-report
-codecov-report:
-	$(if $<,,$(error target codecov-report must have a source file as dependency))
-	$(info uploading Codecov report)
-	@${CODECOV} create-report -c -t ${CODECOV_TOKEN} ${CODECOV_FLAGS} $(addprefix -f ,$^)
--- .make/goreleaser.mk
+++
@@ -1,5 +0,0 @@
-GORELEASER ?= goreleaser
-
-.PHONY: golang-release
-golang-release:
-	@${GORELEASER} release --clean --skip-publish --skip-announce --snapshot
