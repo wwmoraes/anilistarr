5966cb83b6a15192391e6a3b11db70ea4df84364
William Artero <git@artero.dev>
2024-07-24 21:58:26 +0200

    ci: use commit archive URL on nix
    Signed-off-by: William Artero <git@artero.dev>

--- shell.nix
+++ shell.nix
@@ -6,8 +6,8 @@ let
   };
   nixpkgs-unstable = fetchTarball {
     name = "nixos-unstable-a14c5d651cee9ed70f9cd9e83f323f1e531002db";
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-unstable.tar.gz";
-    # url = "https://github.com/NixOS/nixpkgs/archive/a14c5d651cee9ed70f9cd9e83f323f1e531002db.tar.gz";
+    # url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-unstable.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs/archive/a14c5d651cee9ed70f9cd9e83f323f1e531002db.tar.gz";
     sha256 = "1b2dwbqm5vdr7rmxbj5ngrxm7sj5r725rqy60vnlirbbwks6aahb";
   };
   nur = fetchTarball {
