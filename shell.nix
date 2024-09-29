25ca7ad75c59987d40d9d23907af4c712d03f08c
William Artero <git@artero.dev>
2024-09-29 12:12:38 +0200

    chore: updated nix env
    Signed-off-by: William Artero <git@artero.dev>

--- shell.nix
+++ shell.nix
@@ -1,60 +1,57 @@
 let
-  ## nix-prefetch-url --unpack <url>
-  nixpkgs = fetchTarball {
+  pkgs = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
     sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
-  };
-  nixpkgs-unstable = fetchTarball {
+  }) {};
+  unstable = import (fetchTarball {
     name = "nixos-unstable-a14c5d651cee9ed70f9cd9e83f323f1e531002db";
-    # url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-unstable.tar.gz";
     url = "https://github.com/NixOS/nixpkgs/archive/a14c5d651cee9ed70f9cd9e83f323f1e531002db.tar.gz";
     sha256 = "1b2dwbqm5vdr7rmxbj5ngrxm7sj5r725rqy60vnlirbbwks6aahb";
-  };
-  kaizen-src = fetchTarball {
-    name = "kaizen-01fb203b0905ed33b45a562a2cb5e5b6330044a8";
-    url = "https://github.com/wwmoraes/kaizen/archive/01fb203b0905ed33b45a562a2cb5e5b6330044a8.tar.gz";
-    sha256 = "04207l8g0p94jix2brwyhky1cscnd9w6vjn5dzzpfyv71wc2g0qa";
-  };
-  pkgs = import nixpkgs {};
-  unstable = import nixpkgs-unstable {};
-  kaizen = import kaizen-src { inherit pkgs; };
+  }) {};
+  kaizen = import (fetchTarball {
+    name = "kaizen-8075b45edf93d8f95a00958fd3a1cc606ba3405c";
+    url = "https://github.com/wwmoraes/kaizen/archive/8075b45edf93d8f95a00958fd3a1cc606ba3405c.tar.gz";
+    sha256 = "1w6fkd5kqm2l2aij31kd9ddk84phbcir7flrbha6mxw2y4j36z09";
+  }) { inherit pkgs; };
   inherit (pkgs) lib mkShell;
 in mkShell {
-  packages = with pkgs; [
-    # TODO try commitlint-rs
-    curl
-    docker-client
-    editorconfig-checker
-    git
-    go-task
-    goreleaser
-    grype
-    hadolint
-    jq
+  packages = [
+    ## TODO github.com/Khan/genqlient
+    ## TODO github.com/wadey/gocovmerge
+    ## TODO github.com/xo/xo
+    ## TODO try commitlint-rs
     kaizen.go-commitlint
-    lefthook
-    markdownlint-cli
-    oapi-codegen
-    python312Packages.codecov
-    svu
-    typos
+    pkgs.curl
+    pkgs.docker-client
+    pkgs.editorconfig-checker
+    pkgs.git
+    pkgs.go-task
+    pkgs.goreleaser
+    pkgs.grype
+    pkgs.hadolint
+    pkgs.jq
+    pkgs.lefthook
+    pkgs.markdownlint-cli
+    pkgs.oapi-codegen
+    pkgs.python312Packages.codecov
+    pkgs.svu
+    pkgs.typos
     unstable.container-structure-test
     unstable.go
+    unstable.gotests
     unstable.golangci-lint
     unstable.hadolint-sarif
     unstable.sarif-fmt
-    ## TODO github.com/wadey/gocovmerge
-    ## TODO github.com/Khan/genqlient
-    ## TODO github.com/xo/xo
   ] ++ lib.optionals (builtins.getEnv "CI" != "") [ # CI-only
   ] ++ lib.optionals (builtins.getEnv "CI" == "") [ # local-only
-    # fish
     ## TODO pkgsite
-    flyctl
+    kaizen.gopium
+    kaizen.goutline
     kaizen.structurizr-cli
     kaizen.structurizr-site-generatr
-    plantuml
-    redis
+    pkgs.flyctl
+    pkgs.plantuml
+    pkgs.redis
     unstable.gopls
   ];
 }
