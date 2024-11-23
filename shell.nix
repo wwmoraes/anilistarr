05cbbc3a4ab7b990b01950e180aa9acbe273d14f
William Artero <git@artero.dev>
2024-11-23 11:54:05 +0100

    chore: add niv for nix sources management
    Niv allows easy source management and pinning while cleaning up the
    shell imports section.
    
    Signed-off-by: William Artero <git@artero.dev>

--- shell.nix
+++ shell.nix
@@ -1,20 +1,11 @@
 { system ? builtins.currentSystem
+, sources ? import ./nix/sources.nix
 }: let
-	pkgs = import (fetchTarball {
-		url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
-		sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
-	}) {
+	pkgs = import sources.nixpkgs {
 		inherit system;
 		config.packageOverrides = pkgs: {
-			nur = import (builtins.fetchTarball {
-				url = "https://github.com/nix-community/NUR/archive/master.tar.gz";
-				# sha256 = "0s9is965rv1knq17axd9s1y4l4h81d7dw6s0zsy5j6qwyb0kh703";
-			}) { inherit pkgs; };
-			unstable = import (fetchTarball {
-				name = "nixos-unstable-a14c5d651cee9ed70f9cd9e83f323f1e531002db";
-				url = "https://github.com/NixOS/nixpkgs/archive/a14c5d651cee9ed70f9cd9e83f323f1e531002db.tar.gz";
-				sha256 = "1b2dwbqm5vdr7rmxbj5ngrxm7sj5r725rqy60vnlirbbwks6aahb";
-			}) { inherit system pkgs; };
+			nur = import sources.NUR { inherit pkgs; };
+			unstable = import sources.unstable { inherit pkgs; };
 		};
 	};
 	inherit (pkgs) lib mkShell;
@@ -25,6 +16,7 @@ in mkShell {
 	## TODO try commitlint-rs
 	pkgs.editorconfig-checker
 	pkgs.git
+	pkgs.go-junit-report
 	pkgs.go-task
 	pkgs.goreleaser
 	pkgs.grype
@@ -32,24 +24,24 @@ in mkShell {
 	pkgs.jq
 	pkgs.lefthook
 	pkgs.markdownlint-cli
+	pkgs.nur.repos.wwmoraes.codecov-cli-bin
 	pkgs.oapi-codegen
 	pkgs.svu
 	pkgs.typos
 	pkgs.unstable.container-structure-test
 	pkgs.unstable.go
-	pkgs.unstable.go-junit-report
 	pkgs.unstable.golangci-lint
 	pkgs.unstable.hadolint-sarif
 	pkgs.unstable.sarif-fmt
-	pkgs.nur.repos.wwmoraes.codecov-cli-bin
 	] ++ lib.optionals (builtins.getEnv "CI" != "") [ # CI-only
 	] ++ lib.optionals (builtins.getEnv "CI" == "") [ # local-only
 	## TODO pkgsite
 	## TODO https://github.com/xo/xo
-	pkgs.eclint
 	pkgs.curl
 	pkgs.docker-client
+	pkgs.eclint
 	pkgs.flyctl
+	pkgs.niv
 	pkgs.nur.repos.wwmoraes.go-commitlint
 	pkgs.nur.repos.wwmoraes.gopium
 	pkgs.nur.repos.wwmoraes.goutline
@@ -57,6 +49,7 @@ in mkShell {
 	pkgs.nur.repos.wwmoraes.structurizr-site-generatr
 	pkgs.plantuml
 	pkgs.redis
+	pkgs.sqlc
 	pkgs.unstable.delve
 	pkgs.unstable.golangci-lint-langserver
 	pkgs.unstable.gopls
