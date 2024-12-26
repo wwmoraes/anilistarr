d1774bbe67772ddfed5b347877cf7352f3236da6
William Artero <git@artero.dev>
2024-12-26 15:51:34 +0100

    chore: add go-cover-treemap task
    Signed-off-by: William Artero <git@artero.dev>

--- shell.nix
+++ shell.nix
@@ -52,6 +52,7 @@ in mkShell {
 		pkgs.redis
 		pkgs.sqlc
 		pkgs.unstable.delve
+		pkgs.unstable.go-cover-treemap
 		pkgs.unstable.golangci-lint-langserver
 		pkgs.unstable.gopls
 		pkgs.unstable.gotests
